2173dd96f4a473d8d7eaf6f3a2b92c4ca9ab79d2
Manish Kumar Singh <91113108+Manishkumarsingh41@users.noreply.github.com>
2023-11-28 21:16:19 +0530

    weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 weather = input ('What is the forecast for today? (rain/snow/sun)')
 if weather == 'rain':
- print('Remember your umbrella!')
+ print('Remember your umbrella!!')
 elif weather == 'snow':
- print('Remember your wooly gloves!')
+ print('Remember your wooly gloves!!')
 else:
- print('Remember your sunglasses!')
+ print('Remember your sunglasses!!')
